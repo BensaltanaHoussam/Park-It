e8727786cbc77df4e737080e7c656ca7d5c2d839
BensaltanaHoussam <bensaltna07@gmail.com>
2025-03-10 11:51:21 +0000

    Add initial project structure with essential files and configurations

---
+++ app/Models/Parking.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'location',
+        'total_spots',
+        'available_spots',
+    ];
+
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+}
